273a25b18f19357acdd0f232108e1c04b9542ac3
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2022-10-20 13:36:46 +0700

    fix: delete unused import package

--- src/pages/historyorderdetail.jsx
+++ src/pages/historyorderdetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import HeaderWithBackButton from "@Components/Header/HeaderWithBackButton";
 import CustomerDetail from "@Components/HistoryDetail/CustomerDetail";
 import DeliveryDetail from "@Components/HistoryDetail/DeliveryDetail";
