3ff846e47bb126f6d871ccd89f2a9e2e47783fad
Khoerul Umam <khoerul.umam@gotoko.co.id>
2022-10-29 12:33:03 +0700

    update: review october

---
+++ src/api/review.js
@@ -0,0 +1,14 @@
+// Ini nanti di folder API ini kalau mau dikelompokan di dalam
+// masing-masing nama folder, dicek lagi ya
+// Soalnya ini semua fungsi ada di dalam api/auth
+
+// Sama kalau dari saya, instead of membuat nama file satu fungsi
+// bisa juga satu file berisi banyak fungsi. Misalnya
+// profile.js nanti isi nya fungsi untuk get profile sama put profile
+// bisa pake module.exports functionName
+
+// Atau kalau mau kayak gini gak papa, cuma nanti dikelompokan ke satu folder
+// Misal api/auth isinya login dan register
+// api/category isinya getAllCategories
+// api/profile isinya getProfile sama putProfile (prefix kata api di depan filename boleh diabaikan)
+// api/product isinya getAllProducts sama searchProduct
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,8 @@
 import { Navigate } from "react-router-dom";
 
+// Penamaan file kalau bisa dispesifikasiin jangan hanya index
+// misalnya di middleware ini namanya checkLoggedMiddleware.js
+
 export default function middleware(page) {
   try {
     const token = localStorage.getItem("token");
